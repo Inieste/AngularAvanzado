96859fca219c8d7484418d86ad3d74d453bf6c6b
Jose Luis <josledes@hotmail.com>
2019-02-26 09:16:58 +0100

    Sección 7

--- adminpro/src/app/app.module.ts
+++ adminpro/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {ServiceModule} from './services/service.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { RegisterComponent } from './login/register.component';
   imports: [
     BrowserModule,
     APP_ROUTES,
+    FormsModule,
     PagesModule,
     ServiceModule
   ],
